33389f77cfc7337dfbb9bdd0a8dd64166e3b59a1
Aries Thio <aries.thio@hotmail.com>
2023-09-02 15:55:46 +0200

    Updated Makefile

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '067faeb50b8b6c1438309dee8393fe1aaf100479'
+AESD_ASSIGNMENTS_VERSION = 'ff6b28cdc022d9f39baf9cb245d6fae7c4f391f2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
